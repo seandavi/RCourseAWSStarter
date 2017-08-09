a6136cf3271f36d80b70324a5e87a5d3670a9c02
Sean Davis <seandavi@gmail.com>
2017-08-09 18:19:08 -0400

    Working for now

---
+++ install_shiny_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /tmp
+runuser -l shiny Rscript -e 'install.packages("shiny")'
+wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.2.786-amd64.deb
+gdebi -n shiny-server-1.4.2.786-amd64.deb
+# install required packages in R global directory
+sudo R_LIBS_USER=/usr/local/lib/R/library Rscript -e 'install.packages(c("shiny","R6","httr"),repos="http://cloud.r-project.org/",lib="/usr/local/lib/R/library",dependencies=TRUE,Ncpus=32)'
+sudo apt-get install git
+cd /srv/shiny-server
+sudo git clone https://gist.github.com/79d902e3c8124fc90d41820b541071f2.git register
